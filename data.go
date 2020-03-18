fcb33139b17d09d363a5b376d2fdc5341a70c7a9
Paolo Oliveira <paolocmo@gmail.com>
2020-03-18 18:42:31 -0300

    better code organization

--- data.go
+++ data.go
@@ -1,75 +1,81 @@
 package main
 
 import (
-	"fmt"
+	"crypto/md5"
 	"encoding/json"
-  "io/ioutil"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 type DataBase struct {
-    Name      string `json:"databasename"`
-    Tables map[string]map[string]string `json:"tables"`
+	Name   string                       `json:"databasename"`
+	Tables map[string]map[string]string `json:"tables"`
 }
 
 func (d *DataBase) Init(_name string) {
-    d.Name = _name
-    d.Tables = map[string]map[string]string{}
+	d.Name = _name
+	d.Tables = map[string]map[string]string{}
 }
 
 func (d *DataBase) Set(_table string, _key string, _value string) {
-    dt, ok := d.Tables[_table]
-    if !ok {
-        dt = make(map[string]string)
-        d.Tables[_table] = dt
-    }
-    d.Tables[_table][_key]=_value
+	dt, ok := d.Tables[_table]
+	if !ok {
+		dt = make(map[string]string)
+		d.Tables[_table] = dt
+	}
+	d.Tables[_table][_key] = _value
 }
 
-func (d *DataBase) Get(_table string, _key string) (string){
-  if _key == "" {
-    bytes, _ := json.Marshal(d.Tables[_table])
-    return string(bytes)
-  } else {
-    return d.Tables[_table][_key]
-  }
+func (d *DataBase) Get(_table string, _key string) string {
+	if _key == "" {
+		bytes, _ := json.Marshal(d.Tables[_table])
+		return string(bytes)
+	} else {
+		return d.Tables[_table][_key]
+	}
 }
 
 func (d *DataBase) Del(_table string, _key string) {
-     delete(d.Tables[_table], _key)
+	delete(d.Tables[_table], _key)
 }
 
-func (d *DataBase) Dump() (string){
-     bytes, _ := json.MarshalIndent(d,""," ")
-     return string(bytes)
+func (d *DataBase) Dump() string {
+	bytes, _ := json.MarshalIndent(d, "", " ")
+	return string(bytes)
 }
 
-func (d *DataBase) getFileName() (string){
-  return (d.Name + ".JSONdb")
+func (d *DataBase) Hash() string {
+	bytes, _ := json.Marshal(d)
+	return fmt.Sprintf("%x", md5.Sum(bytes))
 }
 
-func (d *DataBase) Load(){
-  _, err := os.Stat(d.getFileName())
-  if os.IsNotExist(err) {
-    file, err := os.Create(d.Name)
-    if err != nil {
-      fmt.Println(err.Error())
-      os.Exit(1)
-    }
-    defer file.Close()
-  }
-  raw, err := ioutil.ReadFile(d.getFileName())
-  if err != nil {
-    fmt.Println(err.Error())
-    os.Exit(1)
-  }
-  json.Unmarshal(raw, &d)
+func (d *DataBase) getFileName() string {
+	return (d.Name + ".JSONdb")
 }
 
-func (d *DataBase) Store(){
-  err := ioutil.WriteFile(d.getFileName(), []byte(d.Dump()), 0644)
-  if err != nil {
-    fmt.Println(err.Error())
-    os.Exit(1)
-  }
+func (d *DataBase) Load() {
+	_, err := os.Stat(d.getFileName())
+	if os.IsNotExist(err) {
+		file, err := os.Create(d.Name)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		defer file.Close()
+	}
+	raw, err := ioutil.ReadFile(d.getFileName())
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	json.Unmarshal(raw, &d)
+}
+
+func (d *DataBase) Store() {
+	err := ioutil.WriteFile(d.getFileName(), []byte(d.Dump()), 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
--- web.go
+++ web.go
@@ -1,46 +1,56 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "strings"
-  "encoding/json"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
 )
 
-
 type infoMsg struct {
-  Type string   `json:"type"`
-  Information string `json:"information"`
+	Type        string `json:"type"`
+	Information string `json:"information"`
 }
 
-func ret2web(w http.ResponseWriter, r *http.Request, itype string, info string){
-  einfo := infoMsg{Type: itype, Information: info}
-  bJSON, err := json.MarshalIndent(einfo,"", " "); if err != nil { log.Println("error:", err) }
-  w.Header().Set("Content-type", "application/json")
-  w.Write(bJSON)
+func ret2web(w http.ResponseWriter, r *http.Request, itype string, info string) {
+	einfo := infoMsg{Type: itype, Information: info}
+	bJSON, err := json.MarshalIndent(einfo, "", " ")
+	if err != nil {
+		log.Println("error:", err)
+	}
+	// ======================================== CORS
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, STORE, LOAD, DUMP")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	// ================================= set as json
+	w.Header().Set("Content-type", "application/json")
+	// ============================= return the data
+	w.Write(bJSON)
 }
 
 func handleRequests(w http.ResponseWriter, r *http.Request) {
-  parms := strings.Split( r.URL.Path[1:], "/" )
-  if r.Method == "GET" && len(parms) > 1 && len(parms[0]) > 0 && len(parms[1]) > 0 {
-    ret2web(w,r,"result", d.Get(parms[0], parms[1]))
-  } else if r.Method == "POST" && len(parms) > 2 && len(parms[0]) > 0 && len(parms[1]) > 0 && len(parms[2]) > 0 {
-    d.Set(parms[0], parms[1], parms[2])
-    ret2web(w,r,"result", "ok")
-  } else if r.Method == "DELETE" && len(parms) > 1 && len(parms[0]) > 0 && len(parms[1]) > 0 {
-    d.Del(parms[0], parms[1])
-    ret2web(w,r,"result", "ok")
-  } else if r.Method == "STORE" {
-    d.Store()
-    ret2web(w,r,"result", "ok")
-  } else if r.Method == "LOAD" {
-    d.Load()
-    ret2web(w,r,"result", "ok")
-  } else if r.Method == "DUMP" {
-    w.Header().Set("Content-type", "application/json")
-    w.Write([]byte(d.Dump()))
-  } else {
-    ret2web(w,r,"error", "Not Implemented yet")
-  }
-
+	parms := strings.Split(r.URL.Path[1:], "/")
+	if r.Method == "GET" && len(parms) > 1 && len(parms[0]) > 0 && len(parms[1]) > 0 {
+		ret2web(w, r, "result", d.Get(parms[0], parms[1]))
+	} else if r.Method == "POST" && len(parms) > 2 && len(parms[0]) > 0 && len(parms[1]) > 0 && len(parms[2]) > 0 {
+		d.Set(parms[0], parms[1], parms[2])
+		ret2web(w, r, "result", "ok")
+	} else if r.Method == "DELETE" && len(parms) > 1 && len(parms[0]) > 0 && len(parms[1]) > 0 {
+		d.Del(parms[0], parms[1])
+		ret2web(w, r, "result", "ok")
+	} else if r.Method == "STORE" {
+		d.Store()
+		ret2web(w, r, "result", "ok")
+	} else if r.Method == "LOAD" {
+		d.Load()
+		ret2web(w, r, "result", "ok")
+	} else if r.Method == "DUMP" {
+		w.Header().Set("Content-type", "application/json")
+		w.Write([]byte(d.Dump()))
+	} else if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	} else {
+		ret2web(w, r, "error", "Not Implemented yet")
+	}
 }
